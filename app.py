60e845480f97c2e74c419ceef615efbc3bcd7c5d
Juliandos <95juliandos@gmail.com>
2025-01-08 09:29:15 -0600

    Primera interacción con selenium en las páginas de librerias

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, jsonify, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,8 +15,12 @@ def index():
 @app.route('/buscar', methods=['POST'])
 def buscar():
     title = request.form.get('title')
+
+    driver = crear_driver()
     datos = {
-        'title': title
+        'status': 'success',
+        'titulo': title,
+        'datos_libro': buscar_libre_libreria(driver, title)
     }
     return jsonify(datos)
 
@@ -23,4 +28,47 @@ def buscar():
 
 def crear_driver():
     chrome_options = webdriver.ChromeOptions()
-    return chrome_options
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+def buscar_libre_libreria(driver, titulo):
+    driver.get('https://www.buscalibre.com.co/')
+
+    # Encuentre el input con name="q" y escriba el titulo
+    input_busqueda = driver.find_element(By.NAME, 'q')
+    input_busqueda.send_keys(titulo)
+    input_busqueda.send_keys(Keys.ENTER)
+
+    time.sleep(5)
+
+    # Encuentre todos los div con la clase CSS "div.box-producto"
+    divs = driver.find_elements(By.CSS_SELECTOR, 'div.box-producto')
+
+    libros = []
+
+    for div in divs:
+        # Extraer el valor del atributo "src" de la primera etiqueta img y clase " lazyloaded"
+        img_url = div.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        # Extraer el valor de este selector CSS "h3.nombre"
+        titulo_libro = div.find_element(By.CSS_SELECTOR, 'h3.nombre').text
+        # Extraer el valor de este selector "div.autor"
+        autor_libro = div.find_element(By.CSS_SELECTOR, 'div.autor').text
+        # # Extraer el valor del div con clases "autor color-dark-grey metas hide-on-hover"
+        otros_datos = div.find_element(By.CSS_SELECTOR, 'div.autor.color-dark-gray.metas.hide-on-hover').text
+        # # Extraer el valor del elemento p con clases "precio-ahora hide-on-hover margin-0 font-size-medium"
+        precio = div.find_element(By.CSS_SELECTOR, 'p.precio-ahora.hide-on-hover.margin-0.font-size-medium').text
+
+
+        libros.append({
+            'img_url': img_url,
+            'titulo': titulo_libro,
+            'autor': autor_libro,
+            'otros_datos': otros_datos,
+            'precio': precio,
+        })
+
+    return libros
