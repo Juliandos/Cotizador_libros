9c10c63f31c11ceaeaf8c5587cbf2e00dac0e1c1
Juliandos <95juliandos@gmail.com>
2025-01-07 22:20:30 -0600

    Ruta para iniciar una busqueda con un titulo

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Ruta post pra generar una busqueda a partir de un titulo que se envia desde el cliente
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    title = request.form.get('title')
+    datos = {
+        'title': title
+    }
+    return jsonify(datos)
