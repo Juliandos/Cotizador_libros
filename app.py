3520c805f1cd630f5c6dee664974d0f9e5fa927a
Juliandos <95juliandos@gmail.com>
2025-01-09 11:34:19 -0600

    Generación targetas librería nacional

--- app.py
+++ app.py
@@ -54,21 +54,17 @@ def buscar_nacional_libreria(driver, titulo):
     input_busqueda.send_keys(titulo)
     input_busqueda.send_keys(Keys.ENTER)
 
-    time.sleep(4)
+    time.sleep(3)
 
-    # # Validar si el section con el ID "noEncontrado" existe
-    # # no_encontrado = driver.find_element(By.ID, 'noEncontrado')
-    # # if no_encontrado:
-    # #     return []
+    # Encuentre todos los section con clase "vtex-product-summary-2-x-container vtex-product-summary-2-x-container--cardsLibros vtex-product-summary-2-x-containerNormal vtex-product-summary-2-x-containerNormal--cardsLibros overflow-hidden br3 h-100 w-100 flex flex-column justify-between center tc"
+    libros_encontrados = driver.find_elements(By.CSS_SELECTOR, 'section.vtex-product-summary-2-x-container.vtex-product-summary-2-x-container--cardsLibros.vtex-product-summary-2-x-containerNormal.vtex-product-summary-2-x-containerNormal--cardsLibros.overflow-hidden.br3.h-100.w-100.flex.flex-column.justify-between.center.tc')
 
-    # # Encuentre todos los div con la clase CSS "vtex-search-result-3-x-galleryItem vtex-search-result-3-x-galleryItem--normal vtex-search-result-3-x-galleryItem--grid pa4"
-    libros_encontrados = driver.find_elements(By.CSS_SELECTOR, 'div.vtex-search-result-3-x-galleryItem vtex-search-result-3-x-galleryItem--normal vtex-search-result-3-x-galleryItem--grid pa4')
-    # libros_encontrados = driver.find_elements(By.CSS_SELECTOR, 'div.box-producto')
-    return libros_encontrados
-    # libros = []
+    libros = []
 
-    # for libro in libros_encontrados:
-    #     libros.append(extraerDatosLibre(libro))
+    for libro in libros_encontrados:
+        libros.append(extraerDatosNacional(libro))
+    
+    return libros
     
     # # Buscar los elementos span que tengan la clase "pagnLink"
     # paginas = driver.find_elements(By.CSS_SELECTOR, 'span.pagnLink')
@@ -149,5 +145,40 @@ def extraerDatosLibre(libro):
         'titulo': titulo_libro,
         'autor': autor_libro,
         'otros_datos': otros_datos,
+        'precio': precio,
+    }
+
+def extraerDatosNacional(libro):
+    # Extraer el valor del atributo href de la primera etiqueta a
+    url_libro = libro.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    # Extraer el valor del atributo "src" de la primera etiqueta img y clase " lazyloaded"
+    img_url = libro.find_element(By.TAG_NAME, 'img').get_attribute('src')
+    # Extraer el valor de este selector CSS "h3.nombre"
+    titulo_libro = libro.find_element(By.CSS_SELECTOR, "h3.vtex-product-summary-2-x-productNameContainer span.vtex-product-summary-2-x-productBrand").text
+    # Extraer el valor de este selector "div.autor"
+    autor_libro = libro.find_element(By.CSS_SELECTOR, 'span.vtex-product-specification-badges-0-x-badgeText.vtex-product-specification-badges-0-x-badgeText--autorCardLibros').text
+    # Extraer el valor del elemento p con clases "precio-ahora hide-on-hover margin-0 font-size-medium"
+    try:
+        # Encuentra los elementos que componen el precio
+        currency_code = libro.find_element(By.CSS_SELECTOR, "span.vtex-product-price-1-x-currencyCode").text
+        currency_integer = libro.find_element(By.CSS_SELECTOR, "span.vtex-product-price-1-x-currencyInteger").text
+        currency_group = libro.find_element(By.CSS_SELECTOR, "span.vtex-product-price-1-x-currencyGroup").text
+        currency_literal = libro.find_element(By.CSS_SELECTOR, "span.vtex-product-price-1-x-currencyLiteral").text
+
+        # Verificar si el espacio está vacío y reemplazarlo por "00"
+        if currency_literal.strip() == "":
+            currency_literal = "00"
+        print(currency_literal)
+        # Componer el precio completo
+        precio = f"{currency_code}{currency_integer}{currency_group}{currency_literal}"
+    except:
+        precio = -1
+
+
+    return {
+        'url_libro': url_libro,
+        'img_url': img_url if img_url else 'https://statics.cdn1.buscalibre.com/no_image/ni9.__RS180x180__.jpg',
+        'titulo': titulo_libro,
+        'autor': autor_libro,
         'precio': precio,
     }
